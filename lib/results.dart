8fc01cf7b095d2e0212bf2b095e6bc0e2b7f0ba6
Ajibola14 <109934367+Ajibola14@users.noreply.github.com>
2023-11-15 15:52:22 +0100

    cleaning up the code

--- lib/results.dart
+++ lib/results.dart
@@ -5,6 +5,10 @@ class ResultsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
   }
 }
